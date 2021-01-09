50951a1d359bbbcf7a85cdc88552e580edb6c882
Chris Grimmett <chris@grimtech.net>
2021-01-09 05:02:54 -0800

    add decider index

---
+++ index.js
@@ -0,0 +1,14 @@
+// determines the functionality of this instance
+// based on the value of env var FUNCTION
+
+require('dotenv').config();
+const envImport = require('common/lib/envImport');
+const f = envImport('FUNCTION');
+
+if (f === 'ripper') {
+  require('./ripper/ripper');
+} else if (f === 'transcoder') {
+  require('./transcoder/transcoder');
+} else if (f === 'builder') {
+  require('./builder/builder');
+}
